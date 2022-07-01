7f1b5975748d6eb7713375b2a48609754a2050fb
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-07-01 23:14:01 +0700

    handler

--- main.go
+++ main.go
@@ -5,15 +5,29 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	// n, err := fmt.Fprintf(w, "Hello, World!")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(fmt.Sprintf("number of bytes written:  %d", n))
+
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+// about page
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About")
+}
+
 func main() {
 	// fmt.Println("Hello, World!")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, World!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("number of bytes written: %d", n))
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server is running on port", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
 }
