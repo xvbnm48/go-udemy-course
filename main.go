a8e5871bbe866eaeb9d7be7774b8431ab6dd4cf4
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-07-01 23:21:56 +0700

    error checking

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -23,10 +24,30 @@ func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "About")
 }
 
+func devide(w http.ResponseWriter, r *http.Request) {
+	f, err := devideValue(100.2, 1.0)
+	if err != nil {
+		fmt.Fprintf(w, "cannot devided by zero")
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f devided by %f", 100.0, 10.0, f))
+
+}
+
+func devideValue(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot devide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
 	// fmt.Println("Hello, World!")
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/devide", devide)
 
 	fmt.Println("Server is running on port", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
