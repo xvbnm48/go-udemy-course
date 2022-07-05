0f981461ca5962d3a74039aa0756f40724177d85
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-07-05 23:32:31 +0700

    add chi

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,21 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/xvbnm48/go-udemy-course/pkg/config"
 	"github.com/xvbnm48/go-udemy-course/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
